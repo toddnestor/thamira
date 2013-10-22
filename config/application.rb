349201d79854f2124d9a6e1745f4a8fcfe54c508
Tommy <complanboy@gmail.com>
2013-10-22 16:31:35 +0530

    Time Zone Chennai

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module Thamira
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Chennai'
+    config.active_record.default_timezone = 'Chennai'
 
     # ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
     #     html = %(<div class="field_with_errors">#{html_tag}</div>).html_safe
