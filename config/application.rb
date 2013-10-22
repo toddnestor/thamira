283b7aee766a36257296ef947cf5553b80024b4d
Tommy <complanboy@gmail.com>
2013-10-22 16:42:38 +0530

    Hopefully Working Time Zone

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,7 @@ module Thamira
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-    config.time_zone = 'Asia/Kolkata'
-    config.active_record.default_timezone = 'Asia/Kolkata'
+    config.time_zone = 'Chennai'
 
     # ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
     #     html = %(<div class="field_with_errors">#{html_tag}</div>).html_safe
