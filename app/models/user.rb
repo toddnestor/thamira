e91e5449347b7d6c0d16050dd433d40a36a563cd
Todd Nestor <todd.nestor@gmail.com>
2014-06-12 14:20:06 +0800

    added service bills relation to try and get that migration to run!

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,13 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :service_bills
+  has_many :payments_bills
+  has_many :cloths_bills
+  has_many :eb_bills
+  has_many :courier_bills
+
   def admin?
   	false
   end
